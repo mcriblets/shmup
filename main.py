af25f1b35d5f2058e97fb561db78b09986fec720
mcriblets <mrobinson3@gatech.edu>
2025-03-26 19:46:09 -0400

    back to basics

--- main.py
+++ main.py
@@ -1,50 +1,149 @@
 import pygame
+import random
 from constants import *
-from player import *
-from enemy import *
+from os import path
 
-def main():
-    print("Let's get started!")
-    pygame.init()
-    pygame.mixer.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("The Wheel of Fate is turning...")
-    clock = pygame.time.Clock()
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Shmup!")
+clock = pygame.time.Clock()
+img_dir = path.join(path.dirname(__file__), 'img')
+player_img = pygame.image.load(path.join(img_dir, 'ships/sazabi.gif')).convert()
+enemies_img = pygame.image.load(path.join(img_dir, 'enemies/corgi_butt.png')).convert()
+bullet_img = pygame.image.load(path.join(img_dir, 'effects/laserRed16.png')).convert()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image = pygame.transform.scale(player_img, (100, 75))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 25
+        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.centerx = SCREEN_WIDTH / 2
+        self.rect.bottom = SCREEN_HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+            
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # kill if it moves off the top of the screen
+        if self.rect.bottom < 0:
+            self.kill()
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = enemies_img
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width *.85 / 2)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+        
+
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > SCREEN_HEIGHT + 10 or self.rect.left < -25 or self.rect.right > SCREEN_WIDTH + 20:
+            self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+            
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+background = pygame.image.load(path.join(img_dir, 'background/nebula-prime.png')).convert()
+background_rect = background.get_rect()
+all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+    # Update
+    all_sprites.update()
     
-    all_sprites = pygame.sprite.Group()
-    player = Player()
-    all_sprites.add(player)
-    bullets = pygame.sprite.Group()
-    mobs = pygame.sprite.Group()
-    for i in range(8):
+    player_hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+    if player_hits:
+        running = False
+        
+    mob_hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in mob_hits:
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
-    
-    alive = True
-    
-    while alive:
-        clock.tick(FPS)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                print("Your fate ends here...")
-                return
-            
-        all_sprites.update()
-        hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
-        
-        hits = pygame.sprite.spritecollide(player, mobs, False)
-        if hits:
-            print("Your fate ends here...")
-            alive = False
-        
-        screen.fill(BLACK)
-        all_sprites.draw(screen)
-        pygame.display.flip()
-    
-if __name__ == "__main__":
-    main()
+
+    # Draw / render
+    screen.fill(BLACK)
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
